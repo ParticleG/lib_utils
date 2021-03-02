01da8597492685396d7574d40505cde4118a4e14
Particle_G <particle_g@outlook.com>
2021-03-03 00:02:16 +0800

    - Add enum class to identify auth status. - Change return type to reduce the coupling. - Commented out the unused websocket versionCode authorizer.

--- utils/Authorizer.cc
+++ utils/Authorizer.cc
@@ -4,220 +4,245 @@
 
 #include <models/App.h>
 #include <models/Auth.h>
+#include <models/Message.h>
 #include <plugins/Configurator.h>
-#include <structures/App.h>
+#include <structures/User.h>
 #include <utils/Authorizer.h>
 #include <utils/Crypto.h>
 
+using namespace drogon;
+using namespace drogon_model;
+using namespace tech::plugins;
 using namespace tech::structures;
 using namespace tech::utils;
-using namespace drogon_model;
-using namespace drogon;
 using namespace std;
 
-bool Authorizer::accessToken(const string &email, const string &accessToken, CloseCode &code, string &reason) {
-    if (email.empty() || accessToken.empty()) {
-        code = CloseCode::kInvalidMessage;
-        reason = "Invalid parameters";
-        return false;
-    }
-    try {
-        orm::Mapper<Techmino::Auth> authMapper(app().getDbClient());
-        auto matchedUsers = authMapper.findBy(Criteria(Techmino::Auth::Cols::_email, CompareOperator::EQ, email));
-        if (matchedUsers.empty()) {
-            code = CloseCode::kInvalidMessage;
-            reason = "User not found";
-            return false;
-        }
-        if (accessToken != matchedUsers[0].getValueOfAccessToken()) {
-            code = CloseCode::kInvalidMessage;
-            reason = "Access Token is incorrect";
-            return false;
-        }
-        if (trantor::Date::now() >
-            trantor::Date::fromDbStringLocal(matchedUsers[0].getValueOfAccessTokenExpireTime())) {
-            code = CloseCode::kInvalidMessage;
-            reason = "Access Token is expired";
-            return false;
-        }
-        return true;
-    } catch (const orm::DrogonDbException &e) {
-        LOG_ERROR << "error:" << e.base().what();
-        code = CloseCode::kUnexpectedCondition;
-        reason = "Internal error";
-        return false;
-    }
-}
-
-bool Authorizer::accessToken(
-        const string &email,
+Authorizer::Status Authorizer::accessToken(
+        const int64_t &id,
         const string &accessToken,
-        HttpStatusCode &code, Json::Value &body
+        Json::Value &result
 ) {
-    if (email.empty() || accessToken.empty()) {
-        code = k400BadRequest;
-        body["message"] = "Invalid parameters";
-        return false;
+    if (id < 0 || accessToken.empty()) {
+        return Authorizer::Status::InvalidComponents;
     }
     try {
         orm::Mapper<Techmino::Auth> authMapper(app().getDbClient());
-        auto matchedUsers = authMapper.findBy(Criteria(Techmino::Auth::Cols::_email, CompareOperator::EQ, email));
-        if (matchedUsers.empty()) {
-            code = k404NotFound;
-            body["message"] = "User not found";
-            return false;
+        auto matchedAuths = authMapper.findBy(Criteria(Techmino::Auth::Cols::__id, CompareOperator::EQ, id));
+        if (matchedAuths.empty()) {
+            return Authorizer::Status::NotFound;
         }
-        if (accessToken != matchedUsers[0].getValueOfAccessToken()) {
-            code = k403Forbidden;
-            body["message"] = "Access Token is incorrect";
-            return false;
+        auto auth = matchedAuths[0];
+        if (accessToken != auth.getValueOfAccessToken()) {
+            return Authorizer::Status::Incorrect;
         }
-        if (trantor::Date::now() >
-            trantor::Date::fromDbStringLocal(matchedUsers[0].getValueOfAccessTokenExpireTime())) {
-            code = k401Unauthorized;
-            body["message"] = "Access Token is expired";
-            return false;
+        if (trantor::Date::now() > trantor::Date::fromDbStringLocal(auth.getValueOfAccessTokenExpireTime())) {
+            return Authorizer::Status::Expired;
         }
-        return true;
+        auto configurator = app().getPlugin<Configurator>();
+        auth.setAccessTokenExpireTime(trantor::Date::now().after(configurator->getAccessExpire()).toDbStringLocal());
+        authMapper.update(auth);
+
+        result["id"] = id;
+
+        return Authorizer::Status::OK;
     } catch (const orm::DrogonDbException &e) {
         LOG_ERROR << "error:" << e.base().what();
-        code = k500InternalServerError;
-        body["message"] = "Internal error";
-        return false;
+        return Authorizer::Status::InternalError;
     }
 }
 
 bool Authorizer::authToken(
-        const string &email,
-        const string &authToken,
-        HttpStatusCode &code,
-        Json::Value &body
-) {
-    if (email.empty() || authToken.empty()) {
-        code = k400BadRequest;
-        body["message"] = "Invalid parameters";
-        return false;
-    }
-    try {
-        orm::Mapper<Techmino::Auth> authMapper(app().getDbClient());
-        auto matchedUsers = authMapper.findBy(Criteria(Techmino::Auth::Cols::_email, CompareOperator::EQ, email));
-        if (matchedUsers.empty()) {
-            code = k404NotFound;
-            body["message"] = "User not found";
-            return false;
-        }
-        if (authToken != matchedUsers[0].getValueOfAuthToken()) {
-            code = k403Forbidden;
-            body["message"] = "Auth Token is incorrect";
-            return false;
-        }
-        if (trantor::Date::now() >
-            trantor::Date::fromDbStringLocal(matchedUsers[0].getValueOfAuthTokenExpireTime())) {
-            code = k401Unauthorized;
-            body["message"] = "Auth Token is expired";
-            return false;
-        }
-        return true;
-    } catch (const orm::DrogonDbException &e) {
-        LOG_ERROR << "error:" << e.base().what();
-        code = k500InternalServerError;
-        body["message"] = "Internal error";
-        return false;
-    }
-}
-
-bool Authorizer::password(const string &email, const string &password, HttpStatusCode &code, Json::Value &body) {
-    if (email.empty() || password.empty()) {
-        code = k400BadRequest;
-        body["message"] = "Invalid parameters";
-        return false;
-    }
-    try {
-        auto matchedUsers = app().getDbClient()->execSqlSync(
-                "select * from auth "
-                "where email = $1 "
-                "and crypt($2, password) = password",
-                email, password
-        );
-        if (matchedUsers.empty()) {
-            code = k403Forbidden;
-            body["message"] = "Email:Password pair is incorrect.";
-            return false;
-        }
-        return true;
-    } catch (const orm::DrogonDbException &e) {
-        LOG_ERROR << "error:" << e.base().what();
-        code = k500InternalServerError;
-        body["message"] = "Internal error";
-        return false;
-    }
-}
-
-bool Authorizer::password(
         const WebSocketConnectionPtr &wsConnPtr,
-        const string &email,
-        const string &password,
+        const shared_ptr<Techmino::Auth> &auth,
         CloseCode &code,
         Json::Value &result
 ) {
-    if (email.empty() || password.empty()) {
-        result["message"] = "Wrong format";
-        result["reason"] = "Required string type 'email' and 'password' in 'data'";
-        return false;
-    }
     try {
-        auto matchedUsers = app().getDbClient()->execSqlSync(
-                "select * from auth "
-                "where email = $1 "
-                "and crypt($2, password) = password",
-                email, password
-        );
-        if (matchedUsers.empty()) {
-            result["message"] = "Email:Password pair is incorrect.";
-            return false;
-        } else {
-            result["message"] = "OK";
-
-            auto configurator = app().getPlugin<tech::plugin::Configurator>();
-            Mapper<Techmino::Auth> authMapper(app().getDbClient());
-            Techmino::Auth auth;
-            auth.setId(matchedUsers[0]["_id"].as<int64_t>());
-            auth.setAuthToken(drogon::utils::getUuid());
-            auth.setAuthTokenExpireTime(trantor::Date::now().after(configurator->getAuthExpire()).toDbStringLocal());
-            authMapper.update(auth);
-
-            auto app = wsConnPtr->getContext<App>();
-            app->setID(auth.getValueOfId());
-            app->setAuthToken(auth.getValueOfAuthToken());
-            wsConnPtr->setContext(app);
-
-            return true;
-        }
+        orm::Mapper<Techmino::Auth> authMapper(app().getDbClient());
+        auto configurator = app().getPlugin<Configurator>();
+        auth->setAuthTokenExpireTime(trantor::Date::now().after(configurator->getAuthExpire()).toDbStringLocal());
+        auth->setAccessToken(Crypto::keccak(drogon::utils::getUuid()));
+        auth->setAccessTokenExpireTime(trantor::Date::now().after(configurator->getAccessExpire()).toDbStringLocal());
+        authMapper.update(*auth);
+        result["message"] = "OK";
+        return true;
     } catch (const orm::DrogonDbException &e) {
-        code = CloseCode::kUnexpectedCondition;
         LOG_ERROR << "error:" << e.base().what();
+        code = CloseCode::kUnexpectedCondition;
         result["message"] = "Internal error";
         return false;
     }
 }
 
-bool Authorizer::versionCode(
-        const WebSocketConnectionPtr &wsConnPtr,
-        const int &versionCode,
-        CloseCode &code,
+Authorizer::Status Authorizer::authToken(
+        const int64_t &id,
+        const string &authToken,
         Json::Value &result
 ) {
-    orm::Mapper<Techmino::App> appMapper(app().getDbClient());
-    auto leastApp = appMapper.orderBy(Techmino::App::Cols::_version_code, SortOrder::DESC).limit(1)
-            .findBy(Criteria(Techmino::App::Cols::_compatible, CompareOperator::EQ, false))[0];
+    if (id < 0 || authToken.empty()) {
+        return Authorizer::Status::InvalidComponents;
+    }
+    try {
+        orm::Mapper<Techmino::Auth> authMapper(app().getDbClient());
+        auto matchedAuths = authMapper.findBy(Criteria(Techmino::Auth::Cols::__id, CompareOperator::EQ, id));
+        if (matchedAuths.empty()) {
+            return Authorizer::Status::NotFound;
+        }
+        auto auth = matchedAuths[0];
+        if (authToken != auth.getValueOfAuthToken()) {
+            return Authorizer::Status::Incorrect;
+        }
+        if (trantor::Date::now() > trantor::Date::fromDbStringLocal(auth.getValueOfAuthTokenExpireTime())) {
+            return Authorizer::Status::Expired;
+        }
+        auto configurator = app().getPlugin<Configurator>();
+        auth.setAuthTokenExpireTime(trantor::Date::now().after(configurator->getAuthExpire()).toDbStringLocal());
+        authMapper.update(auth);
 
-    if (versionCode < leastApp.getValueOfVersionCode()) {
-        code = CloseCode::kViolation;
-        result["message"] = "Outdated version";
-        return false;
-    } else {
+        result["id"] = id;
+
+        return Authorizer::Status::OK;
+    } catch (const orm::DrogonDbException &e) {
+        LOG_ERROR << "error:" << e.base().what();
+        return Authorizer::Status::InternalError;
+    }
+}
+
+Authorizer::Status Authorizer::password(
+        const WebSocketConnectionPtr &wsConnPtr,
+        const string &email,
+        const string &password,
+        Json::Value &result
+) {
+    if (email.empty() || password.empty()) {
+        result["message"] = "Wrong format";
+        result["reason"] = "Empty email or password";
+        return Authorizer::Status::InvalidComponents;
+    }
+    try {
+        auto matchedUsers = app().getDbClient()->execSqlSync(
+                "select * from auth "
+                "where email = $1 "
+                "and crypt($2, password) = password",
+                email, password
+        );
+        if (matchedUsers.empty()) {
+            result["message"] = "Email or Password is incorrect";
+            return Authorizer::Status::Incorrect;
+        }
+
+        auto configurator = app().getPlugin<Configurator>();
+        Mapper<Techmino::Auth> authMapper(app().getDbClient());
+        Techmino::Auth auth;
+        auth.setId(matchedUsers[0]["_id"].as<int64_t>());
+        auth.setAuthToken(drogon::utils::getUuid());
+        auth.setAuthTokenExpireTime(trantor::Date::now().after(configurator->getAuthExpire()).toDbStringLocal());
+        authMapper.update(auth);
+
         result["message"] = "OK";
-        wsConnPtr->setContext(make_shared<App>(versionCode));
-        return true;
+        result["id"] = auth.getValueOfId();
+        result["authToken"] = auth.getValueOfAuthToken();
+        return Authorizer::Status::OK;
+    } catch (const orm::DrogonDbException &e) {
+        LOG_ERROR << "error:" << e.base().what();
+        result["message"] = "Internal error";
+        return Authorizer::Status::InternalError;
+    }
+}
+
+Authorizer::Status Authorizer::password(
+        const string &email,
+        const string &password,
+        Json::Value &result
+) {
+    if (email.empty() || password.empty()) {
+        return Authorizer::Status::InvalidComponents;
+    }
+    try {
+        auto matchedAuths = app().getDbClient()->execSqlSync(
+                "select * from auth "
+                "where email = $1 "
+                "and crypt($2, password) = password",
+                email, password
+        );
+        if (matchedAuths.empty()) {
+            return Authorizer::Status::Incorrect;
+        }
+        auto auth = matchedAuths[0];
+        auto configurator = app().getPlugin<Configurator>();
+        Mapper<Techmino::Auth> authMapper(app().getDbClient());
+        Techmino::Auth newAuth;
+        newAuth.setId(auth["_id"].as<int64_t>());
+        newAuth.setAuthToken(drogon::utils::getUuid());
+        newAuth.setAuthTokenExpireTime(trantor::Date::now().after(configurator->getAuthExpire()).toDbStringLocal());
+        authMapper.update(newAuth);
+
+        result["id"] = newAuth.getValueOfId();
+
+        return Authorizer::Status::OK;
+    } catch (const orm::DrogonDbException &e) {
+        LOG_ERROR << "error:" << e.base().what();
+        return Authorizer::Status::InternalError;
+    }
+}
+
+
+//bool Authorizer::versionCode(
+//        const WebSocketConnectionPtr &wsConnPtr,
+//        const int &versionCode,
+//        CloseCode &code,
+//        Json::Value &result
+//) {
+//    orm::Mapper<Techmino::App> appMapper(app().getDbClient());
+//    auto leastApp = appMapper.orderBy(Techmino::App::Cols::_version_code, SortOrder::DESC).limit(1)
+//            .findBy(Criteria(Techmino::App::Cols::_compatible, CompareOperator::EQ, false))[0];
+//
+//    if (versionCode < leastApp.getValueOfVersionCode()) {
+//        code = CloseCode::kViolation;
+//        result["message"] = "Outdated version";
+//        return false;
+//    } else {
+//        result["message"] = "OK";
+//        wsConnPtr->setContext(make_shared<App>(versionCode));
+//        return true;
+//    }
+//}
+
+Authorizer::Status Authorizer::versionCode(
+        const int &versionCode,
+        Json::Value &result
+) {
+    if (versionCode < 0) {
+        return Authorizer::Status::InvalidComponents;
+    }
+    try {
+        orm::Mapper<Techmino::App> appMapper(app().getDbClient());
+        orm::Mapper<Techmino::Message> messageMapper(app().getDbClient());
+        auto newestApp = appMapper.orderBy(Techmino::App::Cols::_version_code, SortOrder::DESC).limit(1).findAll()[0];
+        auto leastApp = appMapper.orderBy(Techmino::App::Cols::_version_code, SortOrder::DESC).limit(1)
+                .findBy(Criteria(Techmino::App::Cols::_compatible, CompareOperator::EQ, false))[0];
+        auto notice = messageMapper.orderBy(Techmino::Message::Cols::_id, SortOrder::DESC)
+                .findBy(Criteria(Techmino::Message::Cols::_type, CompareOperator::EQ, "notice"))[0];
+
+        if (versionCode < leastApp.getValueOfVersionCode()) {
+            result["newest"]["code"] = newestApp.getValueOfVersionCode();
+            result["newest"]["name"] = newestApp.getValueOfVersionName();
+            result["newest"]["content"] = newestApp.getValueOfVersionContent();
+            result["least"]["code"] = leastApp.getValueOfVersionCode();
+            result["least"]["name"] = leastApp.getValueOfVersionName();
+            result["least"]["content"] = leastApp.getValueOfVersionContent();
+            result["notice"] = notice.getValueOfContent();
+            return Authorizer::Status::Expired;
+        } else {
+            result["versionCode"] = versionCode;
+            result["content"]["newest"]["code"] = newestApp.getValueOfVersionCode();
+            result["content"]["newest"]["name"] = newestApp.getValueOfVersionName();
+            result["content"]["newest"]["content"] = newestApp.getValueOfVersionContent();
+            result["content"]["notice"] = notice.getValueOfContent();
+            return Authorizer::Status::OK;
+        }
+    } catch (const orm::DrogonDbException &e) {
+        LOG_ERROR << "error:" << e.base().what();
+        return Authorizer::Status::InternalError;
     }
 }
--- utils/Authorizer.h
+++ utils/Authorizer.h
@@ -10,46 +10,62 @@
 namespace tech::utils {
     class Authorizer {
     public:
-        static bool accessToken(
-                const std::string &email,
+        enum class Status {
+            OK,
+            InvalidComponents,
+            NotFound,
+            Incorrect,
+            Expired,
+            InternalError,
+        };
+        enum class Type {
+            GetAuthToken,
+            CheckAuthToken,
+            GetAccessToken,
+            CheckAccessToken,
+        };
+    public:
+        static Status accessToken(
+                const int64_t &id,
                 const std::string &accessToken,
-                drogon::CloseCode &code,
-                std::string &reason
-        );
-
-        static bool accessToken(
-                const std::string &email,
-                const std::string &accessToken,
-                drogon::HttpStatusCode &code,
-                Json::Value &body
+                Json::Value &result
         );
 
         static bool authToken(
-                const std::string &email,
-                const std::string &authToken,
-                drogon::HttpStatusCode &code,
-                Json::Value &body
-        );
-
-        static bool password(
                 const drogon::WebSocketConnectionPtr &wsConnPtr,
-                const std::string &email,
-                const std::string &password,
+                const std::shared_ptr<drogon_model::Techmino::Auth> &auth,
                 drogon::CloseCode &code,
                 Json::Value &result
         );
 
-        static bool password(
-                const std::string &email,
-                const std::string &password,
-                drogon::HttpStatusCode &code,
-                Json::Value &body
+        static Status authToken(
+                const int64_t &id,
+                const std::string &authToken,
+                Json::Value &result
         );
 
-        static bool versionCode(
+        static Status password(
                 const drogon::WebSocketConnectionPtr &wsConnPtr,
+                const std::string &email,
+                const std::string &password,
+                Json::Value &result
+        );
+
+        static Status password(
+                const std::string &email,
+                const std::string &password,
+                Json::Value &result
+        );
+
+//        static bool versionCode(
+//                const drogon::WebSocketConnectionPtr &wsConnPtr,
+//                const int &versionCode,
+//                drogon::CloseCode &code,
+//                Json::Value &result
+//        );
+
+        static Authorizer::Status versionCode(
                 const int &versionCode,
-                drogon::CloseCode &code,
                 Json::Value &result
         );
     };
