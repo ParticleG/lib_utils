57d254bba2fa124cad080dd7f97e7f6c5e9ea116
Particle_G <particle_g@outlook.com>
2021-04-11 20:25:35 +0800

    - Add new crypt method

--- utils/crypto.h
+++ utils/crypto.h
@@ -6,10 +6,19 @@
 
 #include <string>
 
-namespace tech::utils {
-    namespace crypto {
-        std::string blake2b(const std::string &source, const unsigned int &divider = 2);
-        std::string keccak(const std::string &source, const unsigned int &divider = 2);
+namespace tech::utils::crypto {
+    namespace panama {
+        std::string generateKey();
+
+        std::string generateIV();
+
+        std::string encrypt(const std::string &source, std::string key, std::string iv);
+
+        std::string decrypt(std::string source, std::string key, std::string iv);
     }
+
+    std::string blake2b(const std::string &source, const unsigned int &divider = 2);
+
+    std::string keccak(const std::string &source, const unsigned int &divider = 2);
 }
 
