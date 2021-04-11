69980e7f487026cfdddc4410d64faf924fba094b
Particle_G <particle_g@outlook.com>
2021-04-11 20:26:53 +0800

    - Remove enum class

--- utils/http.h
+++ utils/http.h
@@ -7,36 +7,34 @@
 #include <drogon/HttpController.h>
 #include <json/json.h>
 
-namespace tech::utils {
-    namespace http {
-        void fromJson(
-                const drogon::HttpStatusCode &code,
-                const Json::Value &body,
-                const std::function<void(const drogon::HttpResponsePtr &)> &callback
-        );
+namespace tech::utils::http {
+    void fromJson(
+            const drogon::HttpStatusCode &code,
+            const Json::Value &body,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
 
-        void fromPlain(
-                const drogon::HttpStatusCode &code,
-                const std::string &body,
-                const std::function<void(const drogon::HttpResponsePtr &)> &callback
-        );
+    void fromPlain(
+            const drogon::HttpStatusCode &code,
+            const std::string &body,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
 
-        void fromHtml(
-                const drogon::HttpStatusCode &code,
-                const std::string &body,
-                const std::function<void(const drogon::HttpResponsePtr &)> &callback
-        );
+    void fromHtml(
+            const drogon::HttpStatusCode &code,
+            const std::string &body,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
 
-        void from(
-                const drogon::HttpStatusCode &code,
-                const drogon::ContentType &type,
-                const std::string &body,
-                const std::function<void(const drogon::HttpResponsePtr &)> &callback
-        );
+    void from(
+            const drogon::HttpStatusCode &code,
+            const drogon::ContentType &type,
+            const std::string &body,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
 
-        std::string toJson(
-                const drogon::HttpRequestPtr &req,
-                Json::Value &result
-        );
-    }
+    std::string toJson(
+            const drogon::HttpRequestPtr &req,
+            Json::Value &result
+    );
 }
--- utils/websocket.h
+++ utils/websocket.h
@@ -6,35 +6,25 @@
 
 #include <drogon/WebSocketController.h>
 
-namespace tech::utils {
-    namespace websocket {
-        enum class Type {
-            App = 0,
-            User = 1,
-            Chat = 2,
-            Play = 3,
-            Stream = 4
-        };
+namespace tech::utils::websocket {
+    std::string fromJson(
+            const Json::Value &value
+    );
 
-        std::string fromJson(
-                const Json::Value &value
-        );
+    std::string toJson(
+            const std::string &rawData,
+            Json::Value &result
+    );
 
-        std::string toJson(
-                const std::string &rawData,
-                Json::Value &result
-        );
+    void initPing(
+            const drogon::WebSocketConnectionPtr &wsConnPtr,
+            const Json::Value &initMessage,
+            const std::chrono::duration<long double> &interval
+    );
 
-        void initPing(
-                const drogon::WebSocketConnectionPtr &wsConnPtr,
-                const Json::Value &initMessage,
-                const std::chrono::duration<long double> &interval
-        );
-
-        void close(
-                const drogon::WebSocketConnectionPtr &webSocketConnectionPtr,
-                drogon::CloseCode _code,
-                const std::string &_reason
-        );
-    }
+    void close(
+            const drogon::WebSocketConnectionPtr &webSocketConnectionPtr,
+            drogon::CloseCode _code,
+            const std::string &_reason
+    );
 }
